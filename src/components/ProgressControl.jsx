ddc1a831a12fd75cb54cd8f18148877d952fc84f
kaihsu12 <kaihsu12@gmail.com>
2023-05-16 14:32:06 +0800

    add if statement to return html tags according to prop

--- src/components/ProgressControl.jsx
+++ src/components/ProgressControl.jsx
@@ -2,34 +2,62 @@ import classes from './style/ProgressControl.module.css';
 import preArrow from '../svg/prev-arrow.svg';
 import nextArrow from '../svg/next-arrow.svg';
 
-function ProgressControl() {
-  return (
-    <section className={classes.controlContainer}>
-      <section class={classes.buttonGroup} data-phase="address">
-        <button className={classes.next}>
-          下一步
-          <img src={nextArrow} alt="" />
-        </button>
+function ProgressControl({ onStepControl, stepNum }) {
+  if (stepNum === 0) {
+    return (
+      <section className={classes.controlContainer}>
+        <section class={classes.buttonGroup} data-phase="address">
+          <button
+            className={classes.next}
+            onClick={onStepControl}
+            data-step="next"
+          >
+            下一步
+            <img src={nextArrow} alt="" data-step="next" />
+          </button>
+        </section>
       </section>
-      <section class={classes.buttonGroup} data-phase="shipping">
-        <button className={classes.prev}>
-          <img src={preArrow} alt="" />
-          上一步
-        </button>
-        <button className={classes.next}>
-          下一步
-          <img src={nextArrow} alt="" />
-        </button>
+    );
+  } else if (stepNum === 1) {
+    return (
+      <section className={classes.controlContainer}>
+        <section class={classes.buttonGroup} data-phase="shipping">
+          <button
+            className={classes.prev}
+            onClick={onStepControl}
+            data-step="prev"
+          >
+            <img src={prevArrow} alt="" data-step="prev" />
+            上一步
+          </button>
+          <button
+            className={classes.next}
+            onClick={onStepControl}
+            data-step="next"
+          >
+            下一步
+            <img src={nextArrow} alt="" data-step="next" />
+          </button>
+        </section>
       </section>
-      <section class={classes.buttonGroup} data-phase="credit-card">
-        <button className={classes.prev}>
-          <img src={preArrow} alt="" />
-          上一步
-        </button>
-        <button className={classes.next}>確認下單</button>
+    );
+  } else if (stepNum === 2) {
+    return (
+      <section className={classes.controlContainer}>
+        <section class={classes.buttonGroup} data-phase="credit-card">
+          <button
+            className={classes.prev}
+            onClick={onStepControl}
+            data-step="prev"
+          >
+            <img src={prevArrow} alt="" data-step="prev" />
+            上一步
+          </button>
+          <button className={classes.next}>確認下單</button>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
 
 export default ProgressControl;
