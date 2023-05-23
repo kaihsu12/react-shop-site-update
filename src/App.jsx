32b8f907ea838df2515fa7fba6185fe8c30ee19c
kaihsu12 <kaihsu12@gmail.com>
2023-05-23 20:16:06 +0800

    add handler function and implement Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useImmer } from 'use-immer';
 import classes from './App.module.css';
 import StepProgress from './components/StepProgress';
 import InfoOne from './components/InfoOne';
@@ -6,9 +7,12 @@ import InfoTwo from './components/InfoTwo';
 import InfoThree from './components/InfoThree';
 import ProgressControl from './components/ProgressControl';
 import Cart from './components/Cart';
+import { CartContext } from './components/CartContext';
 
 function App() {
   const [step, setStep] = useState(0);
+  const items = useContext(CartContext);
+  const [cartItems, setCartItems] = useImmer(items);
 
   function handleBtnControl(e) {
     e.preventDefault();
@@ -16,6 +20,22 @@ function App() {
       setStep(step + 1);
     } else if (e.target.dataset.step === 'prev') {
       setStep(step - 1);
+    }
+  }
+
+  function handleQuantity(e) {
+    if (e.target.dataset.counter === 'plus') {
+      setCartItems((items) => {
+        const itemOne = items.find((t) => t.id === e.target.dataset.count);
+        itemOne.quantity++;
+      });
+    } else if (e.target.dataset.counter === 'minus') {
+      setCartItems((items) => {
+        const itemOne = items.find((t) => t.id === e.target.dataset.count);
+        const index = items.findIndex((t) => t.id === e.target.dataset.count);
+        itemOne.quantity > 0 && itemOne.quantity--;
+        itemOne.quantity === 0 && items.splice(index, 1);
+      });
     }
   }
 
@@ -31,9 +51,11 @@ function App() {
               {step === 2 && <InfoThree />}
             </section>
           </section>
-          <div className={classes.itemsContainer}>
-            <Cart />
-          </div>
+          <CartContext.Provider value={cartItems}>
+            <div className={classes.itemsContainer}>
+              <Cart onCounter={handleQuantity} />
+            </div>
+          </CartContext.Provider>
           <div className={classes.progressPanel}>
             <ProgressControl onStepControl={handleBtnControl} stepNum={step} />
           </div>
