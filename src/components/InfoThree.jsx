26ec7d3fd2c2c6a537b571b51d3fd77fdfda2cbd
kaihsu12 <kaihsu12@gmail.com>
2023-05-24 16:53:52 +0800

    use loop to restructure html

--- src/components/InfoThree.jsx
+++ src/components/InfoThree.jsx
@@ -1,47 +1,52 @@
 import classes from './style/InfoThree.module.css';
+import { useContext } from 'react';
+import { CardContext } from './CardContext';
 
-function InfoThree() {
+function InfoThree({ onCardInput }) {
+  const cardInfo = useContext(CardContext);
   return (
     <form className={classes.formCredit} data-phase="credit-card">
       <h3 className={classes.formTitle}>付款資訊</h3>
       <section className={classes.formBody}>
+        {cardInfo
+          .filter(
+            (input) => input.label === '持卡人姓名' || input.label === '卡號'
+          )
+          .map((input) => {
+            return (
+              <div className={classes.inputLayout}>
+                <div className={classes.inputGroup}>
+                  <div className={classes.inputLabel}>{input.label}</div>
+                  <input
+                    className={classes.inputText}
+                    type="text"
+                    placeholder={input.defaultValue}
+                    onChange={(e) => onCardInput?.(e.target.value, input.label)}
+                  />
+                </div>
+              </div>
+            );
+          })}
+
         <div className={classes.inputLayout}>
-          <div className={classes.inputGroup}>
-            <div className={classes.inputLabel}>持卡人姓名</div>
-            <input
-              className={classes.inputText}
-              type="text"
-              placeholder="John Doe"
-            />
-          </div>
-        </div>
-        <div className={classes.inputLayout}>
-          <div className={classes.inputGroup}>
-            <div className={classes.inputLabel}>卡號</div>
-            <input
-              className={classes.inputText}
-              type="text"
-              placeholder="1111 2222 3333 4444"
-            />
-          </div>
-        </div>
-        <div className={classes.inputLayout}>
-          <div className={classes.inputGroup}>
-            <div className={classes.inputLabel}>有效期限</div>
-            <input
-              className={classes.inputText}
-              type="text"
-              placeholder="MM/YY"
-            />
-          </div>
-          <div className={classes.inputGroup}>
-            <div className={classes.inputLabel}>CVC / CCV</div>
-            <input
-              className={classes.inputText}
-              type="text"
-              placeholder="123"
-            />
-          </div>
+          {cardInfo
+            .filter(
+              (input) =>
+                input.label === '有效期限' || input.label === 'CVC / CCV'
+            )
+            .map((input) => {
+              return (
+                <div className={classes.inputGroup}>
+                  <div className={classes.inputLabel}>{input.label}</div>
+                  <input
+                    className={classes.inputText}
+                    type="text"
+                    placeholder={input.defaultValue}
+                    onChange={(e) => onCardInput?.(e.target.value, input.label)}
+                  />
+                </div>
+              );
+            })}
         </div>
       </section>
     </form>
