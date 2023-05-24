fac77853a928eed6930a025071bdc4864e61fba0
kaihsu12 <kaihsu12@gmail.com>
2023-05-24 16:55:31 +0800

    change array to object

--- src/components/CardContext.jsx
+++ src/components/CardContext.jsx
@@ -1,11 +1,26 @@
 import { createContext } from 'react';
 
-const cardInfo = {
-  firstName: '',
-  lastName: '',
-  number: '',
-  expiration: '',
-  cvv: '',
-};
+const cardData = [
+  {
+    label: '持卡人姓名',
+    defaultValue: 'John Doe',
+    inputValue: '',
+  },
+  {
+    label: '卡號',
+    defaultValue: '1111 2222 3333 4444',
+    inputValue: '',
+  },
+  {
+    label: '有效期限',
+    defaultValue: 'MM/YY',
+    inputValue: '',
+  },
+  {
+    label: 'CVC / CCV',
+    defaultValue: '123',
+    inputValue: '',
+  },
+];
 
-export const CardContext = createContext(cardInfo);
+export const CardContext = createContext(cardData);
