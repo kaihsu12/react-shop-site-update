fc6afca5e3d0ecfe9f077e4c1d980b7914632de8
kaihsu12 <kaihsu12@gmail.com>
2023-05-29 17:58:48 +0800

    add property

--- src/components/CardContext.jsx
+++ src/components/CardContext.jsx
@@ -2,21 +2,25 @@ import { createContext } from 'react';
 
 const cardData = [
   {
+    id: 'holder',
     label: '持卡人姓名',
     defaultValue: 'John Doe',
     inputValue: '',
   },
   {
+    id: 'number',
     label: '卡號',
     defaultValue: '1111 2222 3333 4444',
     inputValue: '',
   },
   {
+    id: 'expiration',
     label: '有效期限',
     defaultValue: 'MM/YY',
     inputValue: '',
   },
   {
+    id: 'cvc',
     label: 'CVC / CCV',
     defaultValue: '123',
     inputValue: '',
